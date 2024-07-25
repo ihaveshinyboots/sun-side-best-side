08f4338feda7086268f6d0822f95f0f18d3329bc
shiny <oeh1996@hotmail.sg>
2024-07-26 02:38:15 +0800

    Update otters size

--- src/LeafletComponent.js
+++ src/LeafletComponent.js
@@ -28,8 +28,8 @@ const FitToBounds = ({ lines }) => {
 const LeafletMapComponent = ({ lines, otterCoordinates }) => {
   const otterMarkerIcon = L.icon({
     iconUrl: otterIcon,
-    iconSize: [32, 32], // Adjust the size of the icon
-    iconAnchor: [16, 32], // Point of the icon which will correspond to marker's location
+    iconSize: [24, 24], // Adjust the size of the icon
+    iconAnchor: [0, 16], // Point of the icon which will correspond to marker's location
     popupAnchor: [0, -32], // Point from which the popup should open relative to the iconAnchor
   });
 
